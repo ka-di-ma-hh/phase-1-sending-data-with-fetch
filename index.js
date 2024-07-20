6f7bce4695c6f31dc227d2dec2674218f98d30ce
ka-di-ma-hh <junewanja8@gmail.com>
2024-07-21 01:39:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-// Add your code here
-function submitData(name, email) {
-	return fetch("http://localhost:3000/users", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json",
-		},
-		body: JSON.stringify({ name: name, email: email }),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			document.body.innerHTML = data.id;
-		})
-		.catch((error) => {
-			document.body.innerHTML = error.message;
-		});
-	;
+class Formatter {
+  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let arr = string.split(' ')
+    let newArr = []
+
+    for(let i in arr){
+      if(i==0){
+        newArr.push(this.capitalize(arr[i]))
+      }
+      else{
+        if(noCap.includes(arr[i])){
+        newArr.push(arr[i])
+        }
+        else{
+          newArr.push(this.capitalize(arr[i]))
+        }
+      
+    }
+  }
+  return newArr.join(" ")
+
+  }
 }
-submitData('john','john@example.com')
